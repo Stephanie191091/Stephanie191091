f2b7bf189098fb48efa7b5fddfa5dda8eed3fac7
Stephanie191091 <stephanie_zoghby@hotmail.com>
2025-06-03 21:18:12 +0300

    Publish 3

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
-print('hellos')
-
-done
+name = input("Name: ")
+age = int(input("age: "))
+GPA = floatinput("Enter you GPA (on a scale 4.0): "))
+field = input ("Field of Interest:")
+graduated = input("have you graduated? ")
+if age < 25 and GPA >= 3.5 and graduated ==
+"yes":
+print("You are eligible for a scholarship.")
+elif age < 30 and GPA >= 2.5:
+print("You are eligible for an internship.")
+else:
+print("Please apply again later.")
